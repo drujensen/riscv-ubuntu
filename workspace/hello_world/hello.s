6f744d27ceac32af64d7a2fdac69dd3210e67836
Dru Jensen <drujensen@gmail.com>
2023-01-25 18:04:57 -0800

    feat: add workspace

---
+++ workspace/hello_world/hello.s
@@ -0,0 +1,27 @@
+	.file	"hello.c"
+	.option pic
+	.text
+	.section	.rodata
+	.align	3
+.LC0:
+	.string	"hello world"
+	.text
+	.align	1
+	.globl	main
+	.type	main, @function
+main:
+	addi	sp,sp,-16
+	sd	ra,8(sp)
+	sd	s0,0(sp)
+	addi	s0,sp,16
+	lla	a0,.LC0
+	call	puts@plt
+	li	a5,0
+	mv	a0,a5
+	ld	ra,8(sp)
+	ld	s0,0(sp)
+	addi	sp,sp,16
+	jr	ra
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0"
+	.section	.note.GNU-stack,"",@progbits
